135bf785f1997330d372c2c43c43095dc1d42c96
Huo-Tian <huotianyuxi@petalmail.com>
2026-04-30 22:46:28 +0800

    v1.3.3-V3

---
+++ R23/[R23B]翻转数位.cpp
@@ -0,0 +1,25 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int t, n, k;
+int a[100100];
+
+int main() {
+    cin >> t;
+    for (; t--; ) {
+        cin >> n >> k;
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            cin >> a[i];
+            if (a[i] == 1) {
+                sum++;
+            }
+        }
+        if (sum == k || ((k - sum) % 2 == 0 && (k >= sum))) {
+            cout << "Yes\n";
+        }
+        else {
+            cout << "No\n";
+        }
+    }
+}
