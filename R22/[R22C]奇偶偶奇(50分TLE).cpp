d6c0f09113e13181410d38235de995249dec5a22
Huo-Tian <huotianyuxi@petalmail.com>
2026-05-01 09:16:29 +0800

    v1.3.3-v4

--- R22/[R22C]奇偶偶奇(50分TLE).cpp
+++ R22/[R22C]奇偶偶奇(50分TLE).cpp
@@ -1,37 +1,56 @@
-#include <bits/stdc++.h>
+﻿#include <bits/stdc++.h>
 using namespace std;
 
 int t, n, a[100010];
 
+vector<int> jsl, jsr, osl, osr;
+
+bool check(int js, int os) {
+    for (int i = 1; i <= js; i++) {
+        for (int j = 1; j <= os; j++) {
+            if (jsl[i] < osl[j] && jsl[i] < osr[j] && jsr[i] > osr[j] && jsr[i] > osl[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 int main() {
     cin >> t;
-    for(; t--; ) {
+    for (; t--; ) {
         cin >> n;
-        for(int i = 1; i <= n; i++) {
+        jsl.clear();
+        jsr.clear();
+        osl.clear();
+        jsr.clear();
+        for (int i = 1; i <= n; i++) {
             cin >> a[i];
         }
         bool flag = false;
         int js = 0, os = 0;
-        int jsl, jsr, osl, osr;
 
-        for(int i = 1, j = n; i <= n && j >= 1 && i <= j; i ++, j --) {
-            if(a[i] % 2 == 1 && a[j] % 2 == 1) {
+        for (int i = 1, j = n; i <= n && j >= 1 && i <= j; i++) {
+            if (a[i] % 2 == 1 && a[j] % 2 == 1) {
                 js++;
-                jsl = i, jsr = j;
+                jsl.push_back(i), jsr.push_back(j);
                 //cout << i << " " << j << endl;
             }
-            if(a[i] % 2 == 0 && a[j] % 2 == 0) {
+            if (a[i] % 2 == 0 && a[j] % 2 == 0) {
                 os++;
-                osl = i, osr = j;
+                osl.push_back(i), osr.push_back(j);
                 //cout << i << " " << j << endl;
             }
-            if(os >= 1 && js >= 1 && jsl < osl && jsr > osr) {
-                cout << "Yes\n";
-                flag = true;
-                break;
+            if (os >= 1 && js >= 1) {
+                if (check(js, os)) {
+                    cout << "Yes\n";
+                    flag = true;
+                    break;
+                }
             }
-        } 
-        if(!flag) {
+            j--;
+        }
+        if (!flag) {
             cout << "No\n";
         }
     }
