2b8c1a9c3ec522eb5b43c25e9e6b8c3d84a30ae4
Huo-Tian <huotianyuxi@petalmail.com>
2026-06-07 12:19:07 +0800

    Dev推送：R64更新
    Dev推送：R64更新
    
    Co-Authored-By: HuoTian2016 <273399415+HuoTian2016@users.noreply.github.com>

---
+++ R64/[R64C]盆栽(40分WA).cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int n, q;
+int a[100010];
+int opt, t, x, v;
+ll  val[200010], last[200010];
+
+int main() {
+    cin >> n >> q;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        val[i] = a[i];
+        last[i] = 0;
+    }
+    for (; q--; ) {
+        cin >> opt >> t >> x;
+        val[x] = max(0LL, val[x] - (t - last[x]));
+        last[x] = t;
+        if (opt == 1) {
+            cin >> v;
+            val[x] += v;
+        } else {
+            cout << val[x] << endl;
+        }
+    }
+}
