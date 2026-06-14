c76bdf86c2e96a5467113f7cdca28367e05d0952
Huo-Tian <huotianyuxi@petalmail.com>
2026-06-14 11:56:39 +0800

    更新 [R50D]三元组2(40分TLE).cpp

--- R50/[R50D]三元组2(40分TLE).cpp
+++ R50/[R50D]三元组2(40分TLE).cpp
@@ -2,28 +2,21 @@
 using namespace std;
 
 int na, nb, nc, d;
-int a[1000010], b[1000010], c[1000010];
+int a[200010], b[200010], c[200010];
 int ans = 0;
 
 int main() {
 	cin >> na >> nb >> nc >> d;
 	for(int i = 1; i <= na; i ++) {
 		cin >> a[i];
-	};
+	}
 	for(int i = 1; i <= nb; i ++) {
 		cin >> b[i];
-	};
+	}
 	for(int i = 1; i <= nc; i ++) {
 		cin >> c[i];
 	}
-	for(int i = 1; i <= na; i ++) {
-		for(int j = 1; j <= nb; j ++) {
-			for(int k = 1; k <= nc; k ++) {
-				if((a[i] <= b[j] && b[j] <= c[k]) && (c[k] - a[i] <= d)) {
-					ans ++;
-				}
-			}
-		}
-	}
-	cout << ans << endl;
+    sort(a + 1, a + na + 1);
+    sort(b + 1, a + nb + 1);
+    sort(c + 1, a + nc + 1);
 }
