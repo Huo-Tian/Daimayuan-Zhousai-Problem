2c5121389406ae4cc10598b19b2add347425caab
Huo-Tian <huotianyuxi@petalmail.com>
2026-06-14 11:49:18 +0800

    更新 [R63D]区间缩小(None).cpp

--- R63/[R63D]区间缩小(None).cpp
+++ R63/[R63D]区间缩小(None).cpp
@@ -11,6 +11,7 @@ int main() {
         cin >> d[i];
     }
     memset(dp, 0, sizeof(dp));
+    dp[0][0] = 1;
     for(int i = 1; i <= m; i ++) {
         for(int j = 0; j <= n; j ++) {
             dp[i][j] = dp[i - 1][j] + dp[i - 1][j - d[i]];
